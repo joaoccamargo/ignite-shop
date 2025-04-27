2b4829b87f1ebf082cf7378c6f0b5920545a2eda
João Camargo <jclaudio00_12@hotmail.com>
2025-04-27 19:55:02 -0300

    added stiches, and global styles, pages...

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import "@/styles/globals.css";
+import { globalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  display: 'swap',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  globalStyles();
+  
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
