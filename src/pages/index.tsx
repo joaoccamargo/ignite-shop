3522981db8e7bd5d8ab1a8c5288e8d02c4cce5d0
João Camargo <jclaudio00_12@hotmail.com>
2025-04-28 07:03:19 -0300

    added Link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import 'keen-slider/keen-slider.min.css'
 import { stripe } from "@/lib/stripe";
 import { GetStaticProps } from "next";
 import Stripe from "stripe";
+import Link from "next/link";
 
 interface HomeProps {
   products: {
@@ -17,7 +18,7 @@ interface HomeProps {
 }
 
 
-export default function Home({products} : HomeProps) {
+export default function Home({ products }: HomeProps) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
@@ -27,15 +28,19 @@ export default function Home({products} : HomeProps) {
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products?.map(product => {
-        return(
-          <Product key={product.id} className="keen-slider__slide">
-          <Image src={product.imageUrl} width={520} height={480} alt="" />
-  
-          <footer>
-            <strong>{product.name}</strong>
-            <span>{product.price}</span>
-          </footer>
-        </Product>
+        return (
+          <Link key={product.id} href={`/product/${product.id}`}>
+            <Product
+              as="a"
+              className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
+
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
