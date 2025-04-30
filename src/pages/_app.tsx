783b5a1f0dfa2ee0f475e9d2b9fc0c65c720d253
João Camargo <jclaudio00_12@hotmail.com>
2025-04-29 21:29:23 -0300

    adjusts and create success page server side props

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,10 @@
 import { globalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
-import { Roboto } from 'next/font/google'
 import logoSvg from '../assets/logo.svg'
 import Image from "next/image";
 import { Container, Header } from "@/styles/pages/app";
 
 globalStyles();
-
-const roboto = Roboto({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  display: 'swap',
-})
 
 export default function App({ Component, pageProps }: AppProps) {  
   return (
@@ -19,9 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <Header>
        <Image src={logoSvg} alt=""/>
     </Header>
-    <main className={roboto.className}>
-      <Component {...pageProps} />
-    </main>
+    <Component  {...pageProps} />
     </Container>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
         <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
       </Head>
-      <body className="antialiased">
+      <body className="antialiased" >
         <Main />
         <NextScript />
       </body>
